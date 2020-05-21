7ecc1f546b5a86cc72db02d6feb21e048c6e3c87
Kelvin Brito <britok30@gmail.com>
2020-05-21 01:54:33 -0400

    first commit

---
+++ Brituber/Controller/LoginController.swift
@@ -0,0 +1,38 @@
+//
+//  LoginController.swift
+//  Brituber
+//
+//  Created by Brito on 5/21/20.
+//  Copyright © 2020 Brito. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+    
+    //MARK - Properties
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "UBER"
+        label.font = UIFont(name: "Avenir-Light", size: 36)
+        label.textColor = UIColor(white: 1, alpha: 0.8)
+        return label
+    }()
+    
+    //MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.init(red: 25/255, green: 25/255, blue: 25/255, alpha: 1)
+        view.addSubview(titleLabel)
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+ }
