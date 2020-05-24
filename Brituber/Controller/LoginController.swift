6efa1982cf6f583da8515fcfd6fe6360f263c6f4
Kelvin Brito <britok30@gmail.com>
2020-05-24 17:24:48 -0400

    login and sign up

--- Brituber/Controller/LoginController.swift
+++ Brituber/Controller/LoginController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginController: UIViewController {
     
@@ -44,6 +45,7 @@ class LoginController: UIViewController {
         let button = AuthButton(type: .system)
         button.setTitle("Log In", for: .normal)
         button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        button.addTarget(self, action: #selector(handleLogin), for: .touchUpInside)
         return button
     }()
     
@@ -71,7 +73,21 @@ class LoginController: UIViewController {
       
     }
     
-    //MARK: -Selector
+    //MARK: -Selectors
+    
+    @objc func handleLogin() {
+        guard let email = emailTextField.text else { return }
+        guard let password = passwordTextField.text else { return }
+        
+        Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
+            if let error = error {
+                print("DEBUG: Failed to log in user in with error \(error.localizedDescription)")
+                return
+            }
+            
+            print ("DEBUG: Successfully logged user in...")
+        }
+    }
     
     @objc func handleShowSignUp(){
         let controller = SignUpController()
